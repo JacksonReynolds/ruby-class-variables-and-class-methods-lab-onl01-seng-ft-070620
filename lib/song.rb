e3ef153dd826f058b39688fffd6251a4684e9e8d
Jackson Reynolds <jwreynold@gmail.com>
2020-07-13 13:32:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,17 @@
 class Song
   attr_reader :name, :artist, :genre
 
+  # Class variables
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  # Instance Methods
   def initialize(song_name, artist, genre)
+    @@count++
+    @@artists << artist
+    @@genres << genre
     @name = song_name
     @artist = artist
     @genre = genre
